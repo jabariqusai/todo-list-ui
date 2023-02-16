2047d1073829a13c9e9faabd1a2ba38d6f5ffd30
Momen-G-Ar <cp.momen@gmail.com>
2023-02-16 13:55:16 +0200

    start integrated nor completed

--- src/hooks/list.hook.ts
+++ src/hooks/list.hook.ts
@@ -1,31 +1,57 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Todo } from '../types/todo';
 
 interface IState {
-  items: Todo.IItem[];
+    items: Todo.IItem[];
 }
 
 const useList = () => {
-  const [state, setState] = useState<IState>({ items: [] });
+    const [state, setState] = useState<IState>({ items: [] });
 
-  const add = (item: Todo.IItem) => setState(state => ({ ...state, items: state.items.concat(item) }));
+    const retrieveList = () => fetch('http://localhost:3001/getTasks', { method: 'GET' })
+        .then(response => response.json() as Promise<Todo.IItem[]>);
+    // .then(res => setState( res ));
 
-  const remove = (id: string) => setState(state => ({ ...state, items: state.items.filter(item => item.id !== id) }));
+    // i have to add useEffect 
+    useEffect(() => {
+        retrieveList();
+    }, []);
+    const add = (item: Todo.IItem) => {
+        setState(state => ({ ...state, items: state.items.concat(item) }));
 
-  const update = (updatedItem: Todo.IItem) => {
-    const updated = [...state.items];
+        const options: RequestInit = {
+            method: 'POST',
+            body: JSON.stringify(item),
+            headers: { 'Content-Type': 'application/json' }
+        };
 
-    for (let i = 0; i < updated.length; ++i) {
-      if (updated[i].id === updatedItem.id) {
-        updated[i] = updatedItem;
-        break;
-      }
-    }
+        fetch('http://localhost:3001/addTask', options)
+            .then(res => {
+                if (res.status === 201)
+                    console.log('Item added successfully');
+                else
+                    console.log('Error', res.status);
+            });
+    };
 
-    setState({ items: updated });
-  };
+    const remove = (id: string) => {
+        setState(state => ({ ...state, items: state.items.filter(item => item.id !== id) }));
+    };
 
-  return { ...state, add, remove, update };
+    const update = (updatedItem: Todo.IItem) => {
+        const updated = [...state.items];
+
+        for (let i = 0; i < updated.length; ++i) {
+            if (updated[i].id === updatedItem.id) {
+                updated[i] = updatedItem;
+                break;
+            }
+        }
+
+        setState({ items: updated });
+    };
+
+    return { ...state, add, remove, update };
 };
 
 export default useList;
