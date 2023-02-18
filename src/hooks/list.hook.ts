35dd36a17d79d02fc7f222af2b86e5076e89b65a
Mariam Hasanat <201011@ppu.edu.ps>
2023-02-19 01:07:16 +0200

    integrate delete and update item , FE and BE

--- src/hooks/list.hook.ts
+++ src/hooks/list.hook.ts
@@ -18,42 +18,62 @@ const useList = () => {
     fetchItems();
   }, []);
 
-// should integrate the post feature
+  // should integrate the post feature
   const add = (item: Todo.IItem) => {
-    const options : RequestInit = {
-      method : 'POST' ,
-      headers :{
+    // options with RequestInit will show the request body which will be send in the fetch function 
+
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
         'Content-Type': 'application/json'
       },
-      body : JSON.stringify(item)
-    }
+      body: JSON.stringify(item)
+    };
 
-    fetch('http://localhost:3001/' , options)
-    .then(res => {
-      if (res.status === 201){
-        console.log("successfully added items ");
-        return fetchItems();
-      }else{
-        console.log("failed added items", res.status);
-        
-      }
-    })
+    fetch('http://localhost:3001/', options)
+      .then(res => {
+        if (res.status === 201) {
+          console.log("successfully added items ");
+          return fetchItems();
+        } else {
+          console.log("failed added items", res.status);
 
-  }
+        }
+      });
 
-  const remove = (id: string) => setState(state => ({ ...state, items: state.items.filter(item => item.id !== id) }));
+  };
+
+  const remove = (id: string) => {
+    fetch(`http://localhost:3001/${id}`, { method: 'DELETE' })
+      .then(res => {
+        if (res.status === 200) {
+          console.log("deleted successfully");
+          return fetchItems();
+        } else {
+          console.log('failed to delete items ', res.status);
+        }
+      });
+  };
 
   const update = (updatedItem: Todo.IItem) => {
-    const updated = [...state.items];
 
-    for (let i = 0; i < updated.length; ++i) {
-      if (updated[i].id === updatedItem.id) {
-        updated[i] = updatedItem;
-        break;
-      }
-    }
+    const options: RequestInit = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updatedItem)
+    };
 
-    setState({ items: updated });
+    fetch(`http://localhost:3001/${updatedItem.id}`, options)
+      .then(res => {
+        if (res.status === 201) {
+          console.log("updated successfully", res.status);
+          return fetchItems();
+        } else {
+          console.log('failed updated', res.status);
+          return fetchItems();
+        }
+      });
+    
   };
 
   return { ...state, add, remove, update };
