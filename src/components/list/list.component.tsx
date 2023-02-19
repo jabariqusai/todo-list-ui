49e1bed8451f0d5dd0c7f344deef77911cb02374
dala_bilal <dala_bilal@gmail.com>
2023-02-19 13:52:33 +0200

    loading color

--- src/components/list/list.component.tsx
+++ src/components/list/list.component.tsx
@@ -11,17 +11,18 @@ const List = (props: IProps) => {
   return (
     <div className={classes.wrapper}>
       <Form onSubmit={list.add} />
-      {list.loading && <span style={{"color":"white"}}>Loading...</span>}
-      <ul>
-        {list.items.map(item => (
-          <Item
-            key={item.id}
-            item={item}
-            remove={list.remove}
-            update={list.update}
-          />
-        ))}
-      </ul>
+      {list.loading ? <span style={{ "color": "white" }}>Loading...</span>
+        : <ul>
+          {list.items.map(item => (
+            <Item
+              key={item.id}
+              item={item}
+              remove={list.remove}
+              update={list.update}
+            />
+          ))}
+        </ul>
+      }
     </div>
   );
 };
