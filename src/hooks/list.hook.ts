bc5b0eff66958b305b50abe661f161eaa6d69bfb
ruba <ruba@gmail.com>
2023-02-19 13:53:41 +0200

    add loading

--- src/hooks/list.hook.ts
+++ src/hooks/list.hook.ts
@@ -1,28 +1,75 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Item from '../components/list/item/item.component';
 import { Todo } from '../types/todo';
 
 interface IState {
   items: Todo.IItem[];
+  loading: boolean;
 }
 
+
+
 const useList = () => {
-  const [state, setState] = useState<IState>({ items: [] });
+  const [state, setState] = useState<IState>({ items: [], loading:true });
 
-  const add = (item: Todo.IItem) => setState(state => ({ ...state, items: state.items.concat(item) }));
+  const retrieveList = () => {
+    setState(oldState =>({...oldState,loading:true}))
+    fetch('http://localhost:3007/', { method: 'GET' })
+      .then(res => res.json() as Promise <Todo.IItem[]>)
+      .then(items => setState(oldState=>({ ...oldState, items})))
+      .catch(err => {alert("sth went wrong")})
+      .finally(()=> setState(oldState => ({...oldState, loading:false})))
+  };
 
-  const remove = (id: string) => setState(state => ({ ...state, items: state.items.filter(item => item.id !== id) }));
+  useEffect(() => {
+    retrieveList();
+  }, []);
 
-  const update = (updatedItem: Todo.IItem) => {
-    const updated = [...state.items];
+  const add = (item: Todo.IItem) => {
+    const options: RequestInit = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(item)
+    };
 
-    for (let i = 0; i < updated.length; ++i) {
-      if (updated[i].id === updatedItem.id) {
-        updated[i] = updatedItem;
-        break;
-      }
-    }
+    fetch(`http://localhost:3007/item`, options)
+      .then(res => {
+        if (res.status === 201) {
+          console.debug('Successfully added item');
+          return retrieveList();
+        } else {
+          console.debug('Failed', res.status);
+        }
+      });
+  };
+  const update = (item: Todo.IItem) => {
+    const options: RequestInit = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(item)
+    };
 
-    setState({ items: updated });
+    fetch(`http://localhost:3007/item/${item.id}`, options)
+      .then(res => {
+        if (res.status === 200) {
+          console.debug('Successfully updated item');
+          return retrieveList();
+        } else {
+          console.debug('Failed', res.status);
+        }
+      });
+  };
+
+  const remove = (id: string) => {
+    fetch(`http://localhost:3007/item/${id}`, { method: 'DELETE' })
+      .then(res => {
+        if (res.status === 200) {
+          console.debug('Successfully updated item');
+          return retrieveList();
+        } else {
+          console.debug('Failed', res.status);
+        }
+      });
   };
 
   return { ...state, add, remove, update };
