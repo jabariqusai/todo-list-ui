af87889704f5f650f4177615afc5733f2fc94b56
yara-sarahna <yarasa2211@gmail.com>
2023-02-19 12:27:45 +0200

    fix problems

--- src/hooks/list.hook.ts
+++ src/hooks/list.hook.ts
@@ -1,35 +1,42 @@
+
 import { useEffect, useState } from 'react';
 import { Todo } from '../types/todo';
-
 interface IState {
   items: Todo.IItem[];
 }
-
 const useList = () => {
   const [state, setState] = useState<IState>({ items: [] });
-
-  const retrieveList = () => {
-    fetch('http://localhost:3001/', { method: 'GET' })
+  const callList = () => {
+    fetch('http://localhost:3003/', { method: 'GET' })
       .then(res => res.json() as Promise<Todo.IItem[]>)
       .then(items => setState({ items }));
   };
-
   useEffect(() => {
-    retrieveList();
+    callList();
   }, []);
-
   const add = (item: Todo.IItem) => {
     const options: RequestInit = {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(item)
     };
-
-    fetch(`http://localhost:3001/`, options)
+    fetch(`http://localhost:3003/list`, options)
       .then(res => {
         if (res.status === 201) {
           console.debug('Successfully added item');
-          return retrieveList();
+          return callList();
+        } else {
+          console.debug('Failed', res.status);
+        }
+      });
+  };
+  const remove = (id: string) => {
+    console.log({id});
+    fetch(`http://localhost:3003/${id}`, { method: 'DELETE' })
+      .then(res => {
+        if (res.status === 200) {
+          console.debug('Successfully updated item');
+          return callList();
         } else {
           console.debug('Failed', res.status);
         }
@@ -41,32 +48,16 @@ const useList = () => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(item)
     };
-
-    fetch(`http://localhost:3001/${item.id}`, options)
+    fetch(`http://localhost:3003/list/${item.id}`, options)
       .then(res => {
         if (res.status === 200) {
           console.debug('Successfully updated item');
-          return retrieveList();
+          return callList();
         } else {
           console.debug('Failed', res.status);
         }
       });
   };
-
-  const remove = (id: string) => {
-    fetch(`http://localhost:3001/${id}`, { method: 'DELETE' })
-      .then(res => {
-        if (res.status === 200) {
-          console.debug('Successfully updated item');
-          return retrieveList();
-        } else {
-          console.debug('Failed', res.status);
-        }
-      });
-  };
-
   return { ...state, add, remove, update };
 };
-
 export default useList;
-
